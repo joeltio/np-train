7d240e3083580f49430619ad9bdb4c0ac8a58145
Joel Tio <joeltiojinhon@gmail.com>
2019-01-07 16:09:58 +0800

    Add login, logout and register

--- api/base/views.py
+++ api/base/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+import django.http as http
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+import django.contrib.auth as django_auth
+
+def create_json_response(success=True, message="", status=200):
+    payload = {
+        "message": message,
+        "success": success,
+    }
+
+    return http.JsonResponse(payload, status=status)
+
+
+@csrf_exempt
+@require_POST
+def login(request):
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
+
+    user = django_auth.authenticate(
+        request, username=username, password=password)
+
+    if user is not None:
+        django_auth.login(request, user)
+
+        return create_json_response()
+    else:
+        return create_json_response(
+            success=False,
+            message="Login failed",
+        )
+
+
+def logout(request):
+    django_auth.logout(request)
+
+    return create_json_response()
+
+
+@csrf_exempt
+@require_POST
+def register(request):
+    username = request.POST.get("username", "")
+    password = request.POST.get("password", "")
+
+    if username and password:
+        django_auth.models.User.objects.create_user(
+            username=username,
+            password=password,
+        )
+
+        return create_json_response()
+    else:
+        return create_json_response(
+            success=False,
+            message="Missing username or password",
+            status=400
+        )
--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,2 +1,4 @@
 ./wait-for-it.sh db:5432
+python manage.py makemigrations
+python manage.py migrate
 uwsgi --ini api_uwsgi.ini
