ada2cef2ca20710325ded416dc5ce4948efc6a5a
Joel Tio <joeltiojinhon@gmail.com>
2019-01-07 17:57:37 +0800

    Add order model

--- api/orders/models.py
+++ api/orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    # A train location, e.g. Bishan
+    destination = models.TextField()
+    # PositiveSmallIntegerField accepts [0, 32767]
+    color = models.PositiveSmallIntegerField()
+    # SmallIntegerField accepts [-32768, 32767]
+    status = models.SmallIntegerField()
+
+    STATUS_NOT_ACTIVE = 0
+    STATUS_ACTIVE = 1
+    STATUS_COMPLETED = 2
+
+    def as_json(self):
+        return {
+            "id": self.id,
+            "destination": self.destination,
+            "color": self.color,
+            "status": self.status,
+        }
