e2a72784b9474e52b338b7d208743c38acb9ee94
Joel Tio <joeltiojinhon@gmail.com>
2019-01-07 16:22:18 +0800

    Fix installed apps using wrong name

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.BaseConfig',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
