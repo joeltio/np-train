909da43ca76180c4ea6f9ce6661f3b8f22d1b6d2
Joel Tio <joeltiojinhon@gmail.com>
2019-01-07 18:01:57 +0800

    Add new_order view

--- api/orders/views.py
+++ api/orders/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from orders.models import Order
+import base.views as base_views
+
+
+@csrf_exempt
+@require_POST
+def new_order(request):
+    # Check if there is a destination and color
+    if ("destination" not in request.POST) or ("color" not in request.POST):
+        return base_views.create_json_response(
+            success=False,
+            message="Missing destination or color",
+            status=400,
+        )
+
+    destination = request.POST["destination"]
+    color = request.POST["color"]
+
+    if not color.isdigit():
+        return base_views.create_json_response(
+            success=False,
+            message="The color is not a non-negative integer",
+            status=400,
+        )
+
+    order = Order.objects.create(
+        destination=destination, color=color,
+        status=Order.STATUS_NOT_ACTIVE)
+
+    return base_views.create_json_response(
+        data={"id": order.pk}
+    )
